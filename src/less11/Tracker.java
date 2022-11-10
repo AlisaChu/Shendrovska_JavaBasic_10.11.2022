d4d40a15b6ef27b01f7f1be42afc8d5b938d1933
AlisaChu <shendrovska89@gmail.com>
2022-11-10 17:50:18 +0100

    homework test

--- src/less11/Tracker.java
+++ src/less11/Tracker.java
@@ -1,4 +1,102 @@
 package less11;
 
 public class Tracker {
+
+    private final String name;
+    private final int dayOfBirth;
+    private final int monthOfBirth;
+    private final int yearOfBirth;
+    private final String email;
+    private final String phone;
+
+    private String surname;
+    private int weight;
+    private int pressure;
+    private long stepsByDay;
+    private int age;
+
+    Tracker (String name, int dayOfBirth, int monthOfBirth,
+            int yearOfBirth, String email, String phone,
+            String surname, int weight, int pressure, long stepsByDay) {
+
+        this.name = name;
+        this.dayOfBirth = dayOfBirth;
+        this.monthOfBirth = monthOfBirth;
+        this.yearOfBirth = yearOfBirth;
+        this.email = email;
+        this.phone = phone;
+        this.surname = surname;
+        this.weight = weight;
+        this.pressure = pressure;
+        this.stepsByDay = stepsByDay;
+        this.age = 2022 - yearOfBirth;
+
+    }
+    public String getName() {
+        return this.name;
+    }
+    public int getDayOfBirth() {
+        return this.dayOfBirth;
+    }
+    public int getMonthOfBirth() {
+        return this.monthOfBirth;
+    }
+    public int getYearOfBirth() {
+        return this.yearOfBirth;
+    }
+    public String getEmail() {
+        return this.email;
+    }
+    public String getPhone() {
+        return this.phone;
+    }
+    public String getSurname() {
+        return this.surname;
+    }
+    public int getWeight() {
+        return this.weight;
+    }
+    public int getPressure() {
+        return this.pressure;
+    }
+    public long getStepsByDay() {
+        return this.stepsByDay;
+    }
+    public int getAge() {
+        return this.age;
+    }
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+    public void setPressure(int pressure) {
+        this.pressure = pressure;
+    }
+    public void setStepsByDay(int stepsByDay) {
+        this.stepsByDay = stepsByDay;
+    }
+
+    // print
+    public void printAccountInfo() {
+        System.out.println(this);
+    }
+    public String toString() {
+
+        return "AccountInfo: "+ " " + "name "+ " " + this.name + " " + "day birth " + " " + this.dayOfBirth + " " + "month birth "
+                + this.monthOfBirth + " " + "year birth " + this.yearOfBirth + " " + "email " + this.email + " "
+                + "phone " + this.phone + " " + "surname " + this.surname + " " + "weight " + this.weight + " " + "pressure " +
+                this.pressure + " " + "stepsByDay " + this.stepsByDay + " " + "age " + this.age + " ";
+    }
 }
+
+
+
+
+
+
+
+
+
+
--- src/less11/TrackerMain.java
+++ src/less11/TrackerMain.java
@@ -1,4 +1,35 @@
 package less11;
 
 public class TrackerMain {
+    public static void main(String[] args) {
+
+        new Tracker("Alisa", 13, 3, 1989, "alisamorgan1303@gmail.com",
+                "+30732195471", "Chu", 49, 92, 21000l).printAccountInfo();
+        new Tracker("Vadym", 8, 2, 1982, "german1488@gmail.com",
+                "+30732514803", "German", 92, 68, 15000l).printAccountInfo();
+        new Tracker("Sergei",10,5,1986,"SergeiAndreev@gmail.com",
+                "+380987151985","Andreev",92,85,30000l).printAccountInfo();
+        new Tracker("Anna",10,12,1987,"VictoriaShanty@gmail.com",
+                "+30732452369","Molly",60,80,6000).printAccountInfo();
+        Tracker Victoria = new Tracker("Victoria", 14, 5, 1988,
+                "VictoriaShanty@gmail.com", "+30732415698", "Voronina", 55, 86, 9000l);
+        Tracker Sem = new Tracker("Sem", 20, 6, 1990,
+                "Sem1990@gmail.com", "+30732596898", "Sidorov", 72, 61, 12000l);
+
+        Victoria.printAccountInfo();
+        Victoria.setSurname("Komanina");
+        Victoria.setWeight(49);
+        Victoria.setPressure(140);
+        Victoria.setStepsByDay(20000);
+        Victoria.printAccountInfo();
+
+        Sem.printAccountInfo();
+        Sem.setSurname("Ivanov");
+        Sem.setWeight(80);
+        Sem.setPressure(120);
+        Sem.setStepsByDay(30000);
+        Sem.printAccountInfo();
+
+    }
 }
+
