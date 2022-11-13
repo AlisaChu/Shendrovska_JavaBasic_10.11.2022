ff7de80666a437284f810a735f408a5909dbbd20
AlisaChu <shendrovska89@gmail.com>
2022-11-13 20:49:55 +0100

    11 hours of hysteria and the code is ready removed minor bugs

--- src/less777/DrinkMachine.java
+++ src/less777/DrinkMachine.java
@@ -2,17 +2,8 @@ package less777;
 
 public enum DrinkMachine {
 
-    COFFEE(1.30), TEA(0.90), LEMONADE(2.0), MOJITO(2.30), MINERAL(1.5), COCA_COLA(1.2);
+    COFFEE, TEA, LEMONADE, MOJITO, MINERAL, COCA_COLA;
 
-    private final double price;
-
-    DrinkMachine(double price) {
-        this.price = price;
-    }
-
-    public double getPrice() {
-        return price;
-    }
 }
 
 
--- src/less777/DrinksMain.java
+++ src/less777/DrinksMain.java
@@ -6,57 +6,57 @@ public class DrinksMain {
 
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        Drinks drinks = new Drinks();
+        try (Scanner sc = new Scanner(System.in)) {
+            Drinks drinks = new Drinks();
 
-        boolean a = true;
-        while (a) {
-            System.out.println("Choose a drink: ");
-            System.out.println(Arrays.toString(DrinkMachine.values()));
-            String choice = sc.nextLine();
+            boolean a = true;
+            while (a) {
+                System.out.println("Choose a drink: ");
+                System.out.println(Arrays.toString(DrinkMachine.values()));
+                String choice = sc.nextLine();
 
-            choice = choice.toUpperCase();
-            DrinkMachine drinksMachine = DrinkMachine.valueOf(choice);
+                choice = choice.toUpperCase();
+                DrinkMachine drinksMachine = DrinkMachine.valueOf(choice);
 
-            switch (drinksMachine) {
-                case COFFEE:
-                    drinks.coffeePrepare();
-                    break;
-                case TEA:
-                    drinks.teaPrepare();
-                    break;
-                case LEMONADE:
-                    drinks.lemonadePrepare();
-                    break;
-                case MOJITO:
-                    drinks.mojitoPrepare();
-                    break;
-                case MINERAL:
-                    drinks.mineralPrepare();
-                    break;
-                case COCA_COLA:
-                    drinks.coca_colaPrepare();
-                default:
-                    System.out.println("ERROR");
-            }
-            System.out.println("Total price " + Drinks.getSum());
+                switch (drinksMachine) {
+                    case COFFEE:
+                        drinks.coffeePrepare();
+                        break;
+                    case TEA:
+                        drinks.teaPrepare();
+                        break;
+                    case LEMONADE:
+                        drinks.lemonadePrepare();
+                        break;
+                    case MOJITO:
+                        drinks.mojitoPrepare();
+                        break;
+                    case MINERAL:
+                        drinks.mineralPrepare();
+                        break;
+                    case COCA_COLA:
+                        drinks.coca_colaPrepare();
+                    default:
+                        System.out.println("ERROR");
+                }
+                System.out.println("Total price " + Drinks.getSum());
 
-            System.out.println("Would you like anything else?  Yes/No");
+                System.out.println("Would you like anything else?  Yes/No");
 
-            boolean moreDrinks = true;
-            while (moreDrinks) {
-                String answer = sc.nextLine();
-                if (answer.equals("No") || answer.equals("NO") || answer.equals("no")) {
-                    moreDrinks = false;
-                    a = false;
-                } else if (answer.equals("Yes") || answer.equals("YES") || answer.equals("yes")) {
-                    moreDrinks = false;
-                } else {
-                    System.out.println("Answer - YES/NO");
+                boolean moreDrinks = true;
+                while (moreDrinks) {
+                    String answer = sc.nextLine();
+                    if (answer.equals("No") || answer.equals("NO") || answer.equals("no")) {
+                        moreDrinks = false;
+                        a = false;
+                    } else if (answer.equals("Yes") || answer.equals("YES") || answer.equals("yes")) {
+                        moreDrinks = false;
+                    } else {
+                        System.out.println("Answer - YES/NO");
 
+                    }
                 }
             }
         }
     }
 }
-
